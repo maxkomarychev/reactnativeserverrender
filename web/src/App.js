0d772ff802b033bbe2b47ff9febc2cd965386ac0
Max Komarychev <maxkomarychev@gmail.com>
2020-03-21 15:06:17 +0200

    web connects to server

--- web/src/App.js
+++ web/src/App.js
@@ -1,8 +1,21 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import io from 'socket.io-client'
 
-function App() {
+class App extends React.Component {
+  constructor() {
+    super()
+    // this.socket = new io("http://localhost", {port:8080})
+  }
+  componentDidMount() {
+    var socket = io('http://localhost:8080');
+    socket.on('news', function (data) {
+      console.log(data);
+      socket.emit('my other event', { my: 'data' });
+    });
+  }
+  render() {
   return (
     <div className="App">
       <header className="App-header">
@@ -24,6 +37,7 @@ function App() {
       </header>
     </div>
   );
+  }
 }
 
 export default App;
